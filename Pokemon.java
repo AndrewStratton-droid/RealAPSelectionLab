2ef8dc8ef75e72b15eb7fc7aaba095aa0b2a3617
kevinshihora <kevinshihora@kevins-mbp.lan>
2020-04-23 14:08:48 -0700

    Finished creating the objects. Finished switch case and combining characteristics

--- Pokemon.java
+++ Pokemon.java
@@ -1,12 +1,38 @@
 
-public class Pokemon {
-
+public class Pokemon extends Generics{
 	
-	public class Pokemon {
+	//declaring variables
+	public static final String type = "Pokemon";
+	public enum KeyType {combination, name, pokedexnum};
+	public static KeyType key = KeyType.combination;
+	public String name;
+	public int pokedexnum;
+	
+	//public class for creating object
+	public Pokemon(String name, int pokedexnum)
+	{
+		this.setType(type);
+		this.name = name; 
+		this.pokedexnum = pokedexnum; 
+	}
+
+	//Creating the object together. Adding all the characteristics to the object.
+	public String toString()
+	{
+		String output="";
+		switch(key) {
+		case name:
+			output += this.name;
+			break;
+		case pokedexnum:
+			output += this.pokedexnum;
+			break;
+		case combination:
+		default:
+			output += type + ": " + this.name  + ", " + this.pokedexnum; 
+		}
+		return output;
 		
-		//declaring variables
-		public static final String type = "Pokemon";
-		public String name;
-		public int pokedexnum;
 	}
 }
+
